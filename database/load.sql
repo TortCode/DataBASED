a1c09e71ce6f4866d38bc893c8fbc6c526b9d619
Teerth Laptop <teerthp2004@gmail.com>
2024-04-21 15:40:52 -0500

    alter load.sql to use relative paths

--- database/load.sql
+++ database/load.sql
@@ -14,67 +14,67 @@ DELETE FROM publisher;
 DELETE FROM user;
 DELETE FROM member;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\author.csv'
+LOAD DATA LOCAL INFILE './author.csv'
 INTO TABLE author
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\library_system.csv'
+LOAD DATA LOCAL INFILE './library_system.csv'
 INTO TABLE library_system
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\library.csv'
+LOAD DATA LOCAL INFILE './library.csv'
 INTO TABLE library
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\publisher.csv'
+LOAD DATA LOCAL INFILE './publisher.csv'
 INTO TABLE publisher
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\user.csv'
+LOAD DATA LOCAL INFILE './user.csv'
 INTO TABLE user
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\publication.csv'
+LOAD DATA LOCAL INFILE './publication.csv'
 INTO TABLE publication
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\member.csv'
+LOAD DATA LOCAL INFILE './member.csv'
 INTO TABLE member
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\loan.csv'
+LOAD DATA LOCAL INFILE './loan.csv'
 INTO TABLE loan
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\owns.csv'
+LOAD DATA LOCAL INFILE './owns.csv'
 INTO TABLE owns
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\checks_out.csv'
+LOAD DATA LOCAL INFILE './checks_out.csv'
 INTO TABLE checks_out
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\user_has_phone_no.csv'
+LOAD DATA LOCAL INFILE './user_has_phone_no.csv'
 INTO TABLE user_has_phone_no
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\publisher_has_phone_no.csv'
+LOAD DATA LOCAL INFILE './publisher_has_phone_no.csv'
 INTO TABLE publisher_has_phone_no
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Vikas\\Downloads\\SQLProject\\library_has_phone_no.csv'
+LOAD DATA LOCAL INFILE './library_has_phone_no.csv'
 INTO TABLE library_has_phone_no
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
